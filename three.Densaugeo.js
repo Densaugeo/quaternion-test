ea7fb479f53fb265097c5e1e03bae5facbebabe0
Densaugeo <>
2023-01-02 00:38:08 -0500

    Enable touch-look rotation on iOS

--- three.Densaugeo.js
+++ three.Densaugeo.js
@@ -515,8 +515,16 @@ export function FreeControls(camera, domElement, options) {
     }
   }
   
-  // Attach devicemotion listener on startup because attaching it during a touchstart event is horribly buggy in FF
-  window.addEventListener('devicemotion', accelHandler);
+  // Attach devicemotion listener on startup because attaching it during a touchstart event is horribly buggy in FF. Except on iPhone, then permission has to be requested (which is also inconsistent)
+  if(typeof DeviceMotionEvent.requestPermission === 'function') {
+    domElement.addEventListener('touchend', () => {
+      DeviceMotionEvent.requestPermission().then(() => {
+        window.addEventListener('devicemotion', accelHandler);
+      });
+    }, { 'once': true });
+  } else {
+    window.addEventListener('devicemotion', accelHandler);
+  }
   
   var gamepads = [];
   
